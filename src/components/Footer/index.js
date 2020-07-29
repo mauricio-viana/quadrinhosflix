2877fdaffba85d304745e360da60de02957297a1
Mauricio Viana <mau_viana@live.com>
2020-07-29 18:28:08 -0300

    Dia 3, components useState

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
+import Logo from '../../assets/image/Logo.png';
 import { FooterBase } from './styles';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="/">
+        <img className="Logo" src={Logo} alt="ComicsFlix logo" />
       </a>
       <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
+        Orgulhosamente criado durante a{' '}
+        <a href="https://www.alura.com.br/">Imersão React da Alura</a>
       </p>
     </FooterBase>
   );
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,21 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
+
+const INITIAL_VALUES = {
+  name: '',
+  description: '',
+  color: '#000000',
+};
 
 export default function CadastroCategoria() {
+  const [categories, setCategories] = useState([]);
+  const [values, setValues] = useState(INITIAL_VALUES);
+
+  const handleChangeInput = (event) => {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCategories([...categories, values]);
+    setValues(INITIAL_VALUES);
+  };
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {values.name}</h1>
 
-      <form>
-        <label>
-          Nome da Categoria:
-          <input type="text" />
-        </label>
+      <form onSubmit={handleSubmit}>
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={handleChangeInput}
+        />
 
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="description"
+          value={values.description}
+          onChange={handleChangeInput}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="color"
+          value={values.color}
+          onChange={handleChangeInput}
+        />
         <button>Cadastrar</button>
       </form>
 
+      <ul>
+        {categories.map((category, index) => {
+          return <li key={index}>{category.name}</li>;
+        })}
+      </ul>
       <Link to="/">Ir para home</Link>
     </PageDefault>
   );
