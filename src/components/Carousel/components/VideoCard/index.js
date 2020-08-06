dcc86697c305f2afeda0d1d867e63e9ec037b98a
Mauricio Viana <mau_viana@live.com>
2020-08-06 18:46:47 -0300

    Ajuste slider

--- src/components/Carousel/components/VideoCard/index.js
+++ src/components/Carousel/components/VideoCard/index.js
@@ -2,24 +2,26 @@ import React from 'react';
 import { VideoCardContainer } from './styles';
 
 function getYouTubeId(youtubeURL) {
-  return youtubeURL
-    .replace(
-      /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/,
-      '$7',
-    );
+  return youtubeURL.replace(
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/,
+    '$7'
+  );
 }
 
-
 function VideoCard({ videoTitle, videoURL, categoryColor }) {
-  const image = `https://img.youtube.com/vi/${getYouTubeId(videoURL)}/hqdefault.jpg`;
+  const image = `https://img.youtube.com/vi/${getYouTubeId(
+    videoURL
+  )}/hqdefault.jpg`;
   return (
     <VideoCardContainer
       url={image}
       href={videoURL}
       target="_blank"
       style={{ borderColor: categoryColor || 'red' }}
-      title={videoTitle}
-    />
+      //title={videoTitle}
+    >
+      <span className="description">{videoTitle}</span>
+    </VideoCardContainer>
   );
 }
 
--- src/components/Carousel/components/VideoCard/styles.js
+++ src/components/Carousel/components/VideoCard/styles.js
@@ -1,31 +1,55 @@
 import styled from 'styled-components';
 
 export const VideoCardContainer = styled.a`
-  border: 2px solid;
-  border-radius: 4px;
   text-decoration: none;
   overflow: hidden;
   cursor: pointer;
   color: white;
   flex: 0 0 298px;
   width: 298px;
-  height: 197px;
+  height: 165px;
   background-image: ${({ url }) => `url(${url})`};
   background-size: cover;
   background-position: center;
-  border-radius: 10px;
+  border-radius: 5px;
   position: relative;
   display: flex;
   align-items: flex-end;
-  padding: 16px;
-
-  transition: opacity .3s;
+  transition: all 0.3s;
+  @media (max-width: 800px) {
+    width: 200px;
+    height: 100px;
+  }
   &:hover,
   &:focus {
-    opacity: .5;
+    transform: scale(0.98);
+    /* box-shadow: 0 0 0 1px var(--white); */
+    outline: none;
+    /* width: 400px; */
+    /* height: 267px; */
   }
-  
   &:not(:first-child) {
     margin-left: 20px;
+  }
+
+  .description {
+    background: rgba(0, 0, 0, 0.8);
+    width: 100%;
+    height: 60px;
+    color: var(--white);
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    transform: translateY(60px);
+    transition: transform linear 200ms;
+    opacity: 0;
+  }
+  &:hover .description,
+  &:focus .description {
+    transform: translateY(0);
+    opacity: 1;
+    @media (max-width: 800px) {
+      display: none;
+    }
   }
 `;
